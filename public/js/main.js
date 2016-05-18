10819a7145d12ea7e2ec2a0a1a5482c2c18f3533
JXSTY <596313217@qq.com>
2016-05-18 17:56:05 +0800

    scroll-bg-v2.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,49 @@
 (function() {
-    $(document).ready(function() {
-        var $level2 = $('#level2');
-        var $showCount = $('.top-count');
 
+    function changeBg(inOption) {
+        var $showCount = $('.top-count');
+        var $level2 = $('#level2');
+        io = inOption/2;
+        $showCount.text(io);
+        if (io<740) {
+            $level2.css("background-position-y", -io);
+        }
+    }
+
+
+    $(document).ready(function() {
         var $showWinHeight = $('.window-height');
         var $windowHeight = $(window).height();
-        $showWinHeight.text($windowHeight);
+        var app = {
+            oTop: document.documentElement.scrollTop || document.body.scrollTop
+        }
 
+        $showWinHeight.text($windowHeight);
         // setTimeout("$('body').scrollTop(100);",0)
         $(document).mousemove(function(e) {
             console.log("X: " + e.pageX + ", Y: " + e.pageY);
         });
 
         $(window).resize(function() {
-          var $showWinHeight = $('.window-height');
-          var $windowHeight = $(window).height();
-          $showWinHeight.text($windowHeight);
+            var $showWinHeight = $('.window-height');
+            var $windowHeight = $(window).height();
+            $showWinHeight.text($windowHeight);
         });
-
         // window.onscroll = function() {
         //     var oTop = document.documentElement.scrollTop || document.body.scrollTop;
         //     console.log("scrollTop=" + oTop);
         //     $showCount.text(oTop);
         //     $level2.css("background-position-y",-oTop);
         // };
-
         $(window).scroll(function() {
-            var oTop = document.documentElement.scrollTop || document.body.scrollTop;
-            console.log("scrollTop=" + oTop);
-            $showCount.text(oTop);
-            $level2.css("background-position-y",-oTop);
+            var ld = $('#level2').offset().top //至顶部的距离
+            var sd = $(document).scrollTop() //页面卷起的距离
+            var wH = $(window).height();
+            var bTop = document.documentElement.scrollTop || document.body.scrollTop;
+            console.log("scrollTop=" + bTop);
+            if (0 < ld - sd < wH) {
+                changeBg(inOption = bTop);
+            }
         });
 
     });
